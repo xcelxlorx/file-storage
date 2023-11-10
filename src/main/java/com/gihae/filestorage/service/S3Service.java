18dea02fe966db087ba854799f43cd5a20bcd54e
xcelxlorx <xcelxlorx@gmail.com>
2023-11-10 12:00:20 +0900

    feat: Add s3 download logic

--- src/main/java/com/gihae/filestorage/service/S3Service.java
+++ src/main/java/com/gihae/filestorage/service/S3Service.java
@@ -1,16 +1,26 @@
 package com.gihae.filestorage.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import com.gihae.filestorage.domain.FileData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,5 +46,18 @@ public class S3Service {
         try (InputStream inputStream = file.getInputStream()) {
             amazonS3.putObject(bucket, fileName, inputStream, metadata);
         }
+    }
+
+    public ResponseEntity<byte[]> download(String originalFileName, String saveFileName) throws IOException {
+        S3Object object = amazonS3.getObject(new GetObjectRequest(bucket, saveFileName));
+
+        S3ObjectInputStream objectContent = object.getObjectContent();
+        byte[] bytes = IOUtils.toByteArray(objectContent);
+        String encodedOriginalFileName = UriUtils.encode(originalFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedOriginalFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(bytes);
     }
 }
