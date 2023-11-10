f81feb2bcfa8d304ba9c60df79434f350d200493
xcelxlorx <xcelxlorx@gmail.com>
2023-11-11 03:21:53 +0900

    feat: Extract save logic in folder

--- src/main/java/com/gihae/filestorage/service/FolderService.java
+++ src/main/java/com/gihae/filestorage/service/FolderService.java
@@ -1,17 +1,17 @@
 package com.gihae.filestorage.service;
 
-import com.gihae.filestorage.controller.dto.FolderRequest;
-import com.gihae.filestorage.controller.dto.FolderResponse;
 import com.gihae.filestorage._core.errors.exception.Exception400;
 import com.gihae.filestorage._core.errors.exception.Exception404;
+import com.gihae.filestorage.controller.dto.FolderRequest;
+import com.gihae.filestorage.controller.dto.FolderResponse;
 import com.gihae.filestorage.domain.Folder;
 import com.gihae.filestorage.repository.FolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -26,23 +26,30 @@ public class FolderService {
     }
 
     @Transactional
-    public void download(){
-
-    }
-
-    @Transactional
     public void create(FolderRequest.SaveDTO saveDTO, Long parentId){
-
-        Optional<Folder> findFolder = folderRepository.findByName(saveDTO.getName());
-        if(findFolder.isPresent()){
+        folderRepository.findByName(saveDTO.getName()).ifPresent(folder -> {
             throw new Exception400("동일한 이름의 폴더가 존재합니다.");
-        }
+        });
 
         Folder parent = folderRepository.findById(parentId).orElseThrow(
                 () -> new Exception404("해당 상위 폴더가 존재하지 않습니다.")
         );
-        Folder folder = Folder.builder().name(saveDTO.getName()).parent(parent).build();
+
+        save(saveDTO, parent);
+    }
+
+    private void save(FolderRequest.SaveDTO saveDTO, Folder parent) {
+        Folder folder = Folder.builder()
+                .name(saveDTO.getName())
+                .parent(parent)
+                .build();
+
         folderRepository.save(folder);
+    }
+
+    @Transactional
+    public void download(){
+
     }
 
     @Transactional
