9ea0ffc0ce8ec6eaa67f5f79971189c3c561e3c2
xcelxlorx <xcelxlorx@gmail.com>
2023-11-11 02:14:42 +0900

    fix: Remove id in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into user_tb(id, email, password, username, role, usage) values(1, 'gihae@naver.com', '$2a$12$G/v/FjrQ4AeRvMl5Ffziz.ouIE1JJvmtrSlMelSV86hYRyRW7aiPC','gihae', 'normal', 0);
+insert into user_tb(email, password, username, role, usage) values('gihae@naver.com', '$2a$12$G/v/FjrQ4AeRvMl5Ffziz.ouIE1JJvmtrSlMelSV86hYRyRW7aiPC','gihae', 'normal', 0);
 insert into folder_tb(id, name) values(0, 'root');
