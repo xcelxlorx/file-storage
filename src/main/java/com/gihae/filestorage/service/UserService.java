9d5baf33154d4505b73d1c00d5bdf833eaed2719
xcelxlorx <xcelxlorx@gmail.com>
2023-11-11 03:20:34 +0900

    feat: Extract save logic in user

--- src/main/java/com/gihae/filestorage/service/UserService.java
+++ src/main/java/com/gihae/filestorage/service/UserService.java
@@ -1,10 +1,11 @@
 package com.gihae.filestorage.service;
 
+import com.gihae.filestorage._core.errors.exception.Exception404;
 import com.gihae.filestorage.controller.dto.UserRequest;
 import com.gihae.filestorage.controller.dto.UserResponse;
-import com.gihae.filestorage._core.errors.exception.Exception400;
-import com.gihae.filestorage._core.errors.exception.Exception404;
+import com.gihae.filestorage.domain.Folder;
 import com.gihae.filestorage.domain.User;
+import com.gihae.filestorage.repository.FolderRepository;
 import com.gihae.filestorage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,18 +18,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final PasswordEncoder passwordEncoder;
+
     private final UserRepository userRepository;
+    private final FolderRepository folderRepository;
 
     @Transactional
     public void join(UserRequest.JoinDTO joinDTO){
         joinDTO.setPassword(passwordEncoder.encode(joinDTO.getPassword()));
+        save(joinDTO);
+    }
+
+    private void save(UserRequest.JoinDTO joinDTO) {
         User user = User.builder()
                 .username(joinDTO.getUsername())
                 .email(joinDTO.getEmail())
                 .role("NORMAL")
                 .password(joinDTO.getPassword())
+                .usage(0L)
                 .build();
+
         userRepository.save(user);
+
+        Folder folder = Folder.builder()
+                .name("root")
+                .parent(null)
+                .build();
+
+        folderRepository.save(folder);
     }
 
     public UserResponse.FindUserDTO findById(Long userId){
