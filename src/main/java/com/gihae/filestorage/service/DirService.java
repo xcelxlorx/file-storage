8e3448f456c9bed0a41bf992022db861726a5e5f
xcelxlorx <xcelxlorx@gmail.com>
2023-11-11 18:37:00 +0900

    feat: Add getResource to dir service

--- src/main/java/com/gihae/filestorage/service/DirService.java
+++ src/main/java/com/gihae/filestorage/service/DirService.java
@@ -1,6 +1,5 @@
 package com.gihae.filestorage.service;
 
-import com.gihae.filestorage._core.errors.exception.Exception500;
 import com.gihae.filestorage.domain.FileData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -28,11 +28,15 @@ public class DirService {
         return fileDir + saveFileName;
     }
 
+    public Resource getResource(String path) throws MalformedURLException {
+        return new UrlResource("file:" + path);
+    }
+
     public void upload(FileData fileData, MultipartFile file) throws IOException {
         file.transferTo(new File(getPath(fileData.getSaveFileName()))); //서버의 파일 시스템에 저장
     }
 
-    public ResponseEntity<Resource> download(String originalFileName, String saveFileName) throws MalformedURLException {
+    public ResponseEntity<Resource> downloadFile(String originalFileName, String saveFileName) throws MalformedURLException {
         UrlResource resource = new UrlResource("file:" + getPath(saveFileName));
         String encodedOriginalFileName = UriUtils.encode(originalFileName, StandardCharsets.UTF_8);
         String contentDisposition = "attachment; filename=\"" + encodedOriginalFileName + "\"";
